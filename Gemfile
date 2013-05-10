a0a6c459ba42567a88f7c8b4fe766e8d3f69d92e
tiseheaini <tiseheaini@gmail.com>
2013-05-10 19:05:23 +0800

    最混乱的一次提交
    添加登录注册

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://ruby.taobao.org/'
 
 gem 'sinatra'
+gem 'rack-flash3'
 gem 'thin'
 gem 'activerecord'
 gem 'sinatra-activerecord'
--- main.rb
+++ main.rb
@@ -1,17 +1,58 @@
-#############
+# encoding: utf-8
+require './model/user'
+#enable :sessions
+use Rack::Session::Pool, :expire_after => 2592000
+use Rack::Flash
 
-get '/:user/print' do
-  "#{params[:user]}main"
+set :erb, :layout_engine => :erb, :layout => :users
+
+get '/' do
+  @user = User.new
+  erb :index
 end
 
-post '/:user/uploads' do
+get '/index' do
+  @user = User.new
+  erb :index
+end
+
+get '/status' do
+  redirect '/' if session[:user_id].blank?
+  @user = User.find(session[:user_id])
+  erb :status
+end
+
+post '/u/sign_in' do
+  @user = User.authentication(params[:users][:u_name], params[:users][:password])
+
+  if @user
+    session[:user_id] = @user.id
+    flash[:sign] = "登录成功"
+    redirect '/status'
+  else
+    flash[:sign] = "登录失败"
+    redirect '/index'
+  end
+end
+
+post '/u/sign_up' do
+  @user = User.new(params[:users]) 
+
+  if @user.save
+    session[:user_id] = @user.id
+    redirect '/index'
+  else
+    erb :new 
+  end
+end
+
+post '/u/:user/uploads' do
 
   params[:info].each do |file_name, file|
     file_path = File.join(File.expand_path('.'), 'assets', file_name)
     FileUtils.makedirs(File.dirname(file_path)) if !File.directory?(File.dirname(file_path))
     file_cont = file[:tempfile].read
     File.open(file_path, "a") {|f| f.puts file_cont } 
-
   end
   "success"
 end
