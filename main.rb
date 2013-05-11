eeed17a507ba076c87d2bb0a7784d52fde0d8caf
tiseheaini <tiseheaini@gmail.com>
2013-05-11 15:35:31 +0800

    改变文件保存问题为 /var/public

--- main.rb
+++ main.rb
@@ -49,7 +49,7 @@ end
 post '/u/:user/uploads' do
 
   params[:info].each do |file_name, file|
-    file_path = File.join(File.expand_path('.'), 'assets', file_name)
+    file_path = File.join('/var', 'public', params[:user], file_name)
     FileUtils.makedirs(File.dirname(file_path)) if !File.directory?(File.dirname(file_path))
     file_cont = file[:tempfile].read
     File.open(file_path, "a") {|f| f.puts file_cont } 
