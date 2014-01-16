eb562b8aa12ced77396c2b901b0a90d91de9df10
tise.c <tise.c@gmail.com>
2014-01-16 13:13:00 +0800

    更改用户验证

--- main.rb
+++ main.rb
@@ -1,11 +1,14 @@
 # encoding: utf-8
 #enable :sessions
+get '/' do
+  "hello sinatra!"
+end
 
 post '/u/:user/uploads' do
 
   pass = YAML.load_file(File.join(Dir.pwd, 'rep-asset.yml'))
 
-  if params[:password] == pass[:password]
+  if params[:password] == pass['rep-asset']['password']
     params[:info].each do |file_name, file|
       file_path = File.join('/var', 'public', params[:user], file_name)
       FileUtils.makedirs(File.dirname(file_path)) if !File.directory?(File.dirname(file_path))
@@ -13,7 +16,7 @@ post '/u/:user/uploads' do
       File.open(file_path, "a") {|f| f.puts file_cont } 
     end
   else
-    halt 500, 'go away!'
+    halt 499, 'go away!go away!'
   end
   "success"
 end
