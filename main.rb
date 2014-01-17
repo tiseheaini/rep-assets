0410b9117a71d814de48f770a94c6c5d171f8cf4
tise.c <tise.c@gmail.com>
2014-01-17 17:28:44 +0800

    接收图片 api

--- main.rb
+++ main.rb
@@ -1,7 +1,27 @@
 # encoding: utf-8
 #enable :sessions
 get '/' do
-  "hello sinatra!"
+  "hello sinatra!!!"
+end
+
+post '/u/:user/picture_save_as' do
+  as_params = params[:info]  ## 原来的 params 信息
+  permalink =  params[:permalink]
+  episode   =  as_params[:cartoon_pic][:episode].to_s
+  cartoon_work_id = as_params[:cartoon_pic][:cartoon_work]
+  episode = as_params[:cartoon_pic][:episode]
+  cartoon_page = 0
+
+  as_params[:cartoon_pic][:pictures].each do |key,picture|
+    next if picture.empty?
+    cartoon_page += 1
+    file_name = cartoon_page.to_s + File.extname(picture[:filename]).downcase
+    file_path = File.join('/var', 'public', 'uploads', 'single', permalink, episode, file_name)
+    FileUtils.makedirs(File.dirname(file_path)) if !File.directory?(File.dirname(file_path))
+    File.open(file_path, 'wb'){ |pic| pic.write(picture[:tempfile].read); picture[:tempfile].close }
+  end
+
+  [200,{"Content-Type" => "text/plain"}, ["Hello World"]]
 end
 
 post '/u/:user/uploads' do
